9cee2bd2f292a541bd7171114f336b70919beb77
git-utkarshdwivedi <udutkarsh097@gmail.com>
2024-05-28 11:21:04 +0530

    pull change done api

--- backend/config/generatToken.js
+++ backend/config/generatToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expriresIN: "30d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET);
 };
 
 module.exports = generateToken;
